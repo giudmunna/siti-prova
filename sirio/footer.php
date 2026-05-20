938a2c02cf74745f956bfbdbb65fc8c75ad90718
giudmunna <163149915+giudmunna@users.noreply.github.com>
2026-05-20 12:04:12 +0200

    Delete sirio directory

--- sirio/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <p>&copy; <?= date("Y") ?> <?= $content['site']['name'] ?> | <?= $content['site']['address'] ?></p>
-</footer>
-
-<script src="assets/js/script.js"></script>
-</body>
-</html>
